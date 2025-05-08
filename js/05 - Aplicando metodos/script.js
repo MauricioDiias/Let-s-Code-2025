79c23b02da80a1d49e916482c711c54db9a2b307
MauricioDiias <mauriciod664@gmail.com>
2025-05-08 20:10:00 -0300

    metodos

---
+++ js/05 - Aplicando metodos/script.js
@@ -0,0 +1,92 @@
+// Principais métodos strings
+
+const nome = "javascript";
+
+console.log(nome.length);
+// deixar as letras maiúsculas
+console.log(nome.toLocaleUpperCase());
+// deixar as letras minúsculas
+console.log(nome.toLowerCase());
+
+const sobreNome = " Dias ";
+console.log(sobreNome);
+//remove os espaços do inicio e do fim
+console.log(sobreNome.trim());
+
+const nomeCompleto = "mauricio dias";
+
+console.log(nomeCompleto.split(""));
+
+// verifica se existe
+console.log(nomeCompleto.split("").includes(""));
+
+const nomes = ["bia", "francisco", "pedro", "marcela", "fabricio"];
+
+nomes.push("marcelo");
+
+let addNome = "bia";
+
+if (nomes.includes(addNome)) {
+  console.log("Esse nome já existe!");
+} else {
+  nomes.push(addNome);
+}
+
+console.log(nomes);
+
+// for (let i = 0; i < nomes.length; i++) {
+//   console.log(nomes[i]);
+// }
+
+// Principais métodos array
+
+// map
+
+nomes.map((nome) => {
+  console.log(nome);
+});
+
+const idades = [2, 6, 20, 15, 31, 17, 40, 29];
+
+const dobrar = idades.map((idade) => {
+  return idade * 2;
+});
+
+console.log(dobrar);
+
+//fillter
+
+const maiorIdade = idades.filter((idade) => {
+  return idade > 18;
+});
+console.log(maiorIdade);
+
+// sort()
+
+const idadeOrdenada = idades.sort((a, b) => a - b);
+
+console.log(idadeOrdenada);
+
+// array reverso
+const reverso = idadeOrdenada.reverse();
+
+console.log(reverso);
+
+// remove e adiciona em uma posição do array
+console.log(nomes);
+const nomeRetirado = nomes.splice(1, 1, "dias", "fabio");
+console.log("Nomes Retirados:" + nomeRetirado);
+
+console.log(nomes);
+
+const index = nomes.indexOf("pedro");
+console.log(index);
+const novoNome = "mauricio";
+
+const nomeRemovido = nomes.splice(index, 1, novoNome);
+
+console.log(nomes);
+
+//slice trás apartir do indice
+
+console.log(nomes.slice(3));
